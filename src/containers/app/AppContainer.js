cd027b108e7b0f09895850f81e15e7eec3fd625f
Mike <perrotta@neosensory.com>
2019-11-11 22:50:07 -0800

    Create about page

--- src/containers/app/AppContainer.js
+++ src/containers/app/AppContainer.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import AppHomePage from './AppHomePage';
+import AppAboutPage from './AppAboutPage';
+import * as Routes from '../../core/router/Routes';
 
 const AppContainer = () => (
   <Switch>
+    <Route path={Routes.ABOUT} component={AppAboutPage} />
     <Route component={AppHomePage} />
   </Switch>
 );
