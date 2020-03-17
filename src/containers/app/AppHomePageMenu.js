977bb3bcc595a1c60866ea3be7989107ccfe782f
Mike <perrotta@neosensory.com>
2020-03-17 13:36:03 -0700

    make main page menu sticky

--- src/containers/app/AppHomePageMenu.js
+++ src/containers/app/AppHomePageMenu.js
@@ -1,22 +1,44 @@
 // @flow
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { NavHashLink } from 'react-router-hash-link';
 
 import { NEUTRALS } from '../../core/styles/Colors';
 import * as Routes from '../../core/router/Routes';
 
-const Menu = styled.div`
+const StickyWrapper = styled.div`
   align-items: center;
-  background-color: ${NEUTRALS.DARK_GRAY};
+  background-color: ${NEUTRALS.WHITE};
   bottom: 0;
   display: flex;
+  height: ${(props) => props.height}px;
   justify-content: center;
-  padding: 12px;
   position: absolute;
   width: 100%;
 `;
+
+const Menu = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  width: 100%;
+  z-index: 200;
+
+  ${(props) => (props.isSticky ? css`
+    background-color: ${NEUTRALS.ALMOST_DARK_GRAY};
+    position: fixed;
+    top: 0;
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
+    ` : `
+    background-color: ${NEUTRALS.DARK_GRAY};
+    bottom: 0;
+    position: absolute;
+    box-shadow: none;
+    `
+)}
+`
 
 const getLinkStyles = () => (
   css`
@@ -25,7 +47,7 @@ const getLinkStyles = () => (
     font-style: normal;
     font-weight: 200;
     line-height: 30px;
-    margin: 0px 80px;
+    margin: 12px 80px;
     text-decoration: none;
 
    &:hover {
@@ -39,19 +61,42 @@ const MenuHashLink = styled(NavHashLink)`
   ${getLinkStyles}
 `;
 
-const AppHeader = () => (
-  <Menu>
-    <MenuHashLink
-        smooth
-        to={Routes.PORTFOLIO}>
-      projects
-    </MenuHashLink>
-    <MenuHashLink
-        smooth
-        to={Routes.ABOUT}>
-      about
-    </MenuHashLink>
-  </Menu>
-);
+const AppHeader = () => {
+  const [isSticky, setStickiness] = useState(false);
+  const [menuHeight, setMenuHeight] = useState(80);
+  const ref = useRef({});
+  const menuRef = useRef({});
+  const handleScroll = () => {
+    if (ref.current) {
+      setStickiness(ref.current.getBoundingClientRect().top <= 0);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll);
+    };
+  }, []);
+  useEffect(() => {
+    setMenuHeight(menuRef.current.offsetHeight);
+  }, []);
+
+  return (
+    <StickyWrapper ref={ref} height={menuHeight}>
+      <Menu isSticky={isSticky} ref={menuRef}>
+        <MenuHashLink
+            smooth
+            to={Routes.PORTFOLIO}>
+          projects
+        </MenuHashLink>
+        <MenuHashLink
+            smooth
+            to={Routes.ABOUT}>
+          about
+        </MenuHashLink>
+      </Menu>
+    </StickyWrapper>
+  );
+};
 
 export default AppHeader;
--- src/core/styles/Colors.js
+++ src/core/styles/Colors.js
@@ -2,6 +2,7 @@ export const NEUTRALS = {
   BLACK: '#000',
   LIGHT_GRAY: '#F1EFEB',
   MED_LIGHT_GRAY: '#E8E5E1',
+  ALMOST_DARK_GRAY: '#3E4241',
   DARK_GRAY: '#282D2B',
   OFF_WHITE: '#F0EFEF',
   WHITE: '#fff',
