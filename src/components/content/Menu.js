3675520ee012e5f4a8547f81ce25d75cbbe3d8d1
Mike <perrotta@neosensory.com>
2020-03-17 14:57:45 -0700

    Create sticky menu in project page

--- src/components/content/Menu.js
+++ src/components/content/Menu.js
@@ -1,7 +1,7 @@
 // @flow
 
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useRef, useState } from 'react';
+import styled, { css } from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import { NavHashLink } from 'react-router-hash-link';
 
@@ -9,6 +9,10 @@ import { NEUTRALS } from '../../core/styles/Colors';
 import * as Routes from '../../core/router/Routes';
 
 /* styled components */
+const StickyWrapper = styled.div`
+  width: 100%;
+`;
+
 const Content = styled.div`
   align-items: center;
   background: ${NEUTRALS.DARK_GRAY};
@@ -17,10 +21,20 @@ const Content = styled.div`
   font-size: 20px;
   font-weight: 200;
   justify-content: space-between;
-  opacity: 0.9;
   padding: 15px 20px;
   text-align: center;
   width: 100%;
+  z-index: 200;
+
+  ${(props) => (props.isSticky ? css`
+  opacity: 0.975;
+  position: fixed;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.4);
+  ` : `
+  opacity: 0.85;
+  position: absolute;
+  box-shadow: none;
+  `)}
 `;
 
 const MenuSubArea = styled.div``;
@@ -50,16 +64,34 @@ const HashLink = styled(NavHashLink)`
 `;
 
 /* react component */
-const Menu = () => (
-  <Content>
-    <MenuSubArea>
-      <Link to={Routes.ROOT}> Mike Perrotta </Link>
-    </MenuSubArea>
-    <MenuSubArea>
-      <HashLink smooth to={Routes.PORTFOLIO}> projects </HashLink>
-      <HashLink smooth to={Routes.ABOUT}> about </HashLink>
-    </MenuSubArea>
-  </Content>
-);
+const Menu = () => {
+  const [isSticky, setStickiness] = useState(false);
+  const ref = useRef({});
+  const handleScroll = () => {
+    if (ref.current) {
+      setStickiness(ref.current.getBoundingClientRect().top < 0);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll);
+    };
+  }, []);
+
+  return (
+    <StickyWrapper ref={ref}>
+      <Content isSticky={isSticky}>
+        <MenuSubArea>
+          <Link to={Routes.ROOT}> Mike Perrotta </Link>
+        </MenuSubArea>
+        <MenuSubArea>
+          <HashLink smooth to={Routes.PORTFOLIO}> projects </HashLink>
+          <HashLink smooth to={Routes.ABOUT}> about </HashLink>
+        </MenuSubArea>
+      </Content>
+    </StickyWrapper>
+  );
+};
 
 export default Menu;
