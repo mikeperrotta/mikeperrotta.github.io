49e94259009c846582fbb67d2c6d7b165baf585c
Mike <perrotta@neosensory.com>
2020-03-18 23:32:35 -0700

    rename variables

--- src/core/router/Routes.js
+++ src/core/router/Routes.js
@@ -9,5 +9,5 @@ export const DESK = `${PROJECTS_ROOT}/desk`;
 export const EMPIRE_APP_EXTERNAL_LINK = 'https://apps.apple.com/us/app/empire-party-game/id1494647723'
 export const EMPIRE_APP = `${PROJECTS_ROOT}/empire-app`
 export const MEDITATION_ARTICLE = 'https://www.medium.com/better-humans/shaping-your-meditation-through-brain-science-ddd33b0fe2ad?source=friends_link&sk=8bc68d28a599bd097120fef820bb8c54';
-export const SHOE_RACK = `${PROJECTS_ROOT}/shoerack`;
-export const SPICE_RACK = `${PROJECTS_ROOT}/spicerack`;
+export const SHOE_RACK = `${PROJECTS_ROOT}/shoe-rack`;
+export const SPICE_RACK = `${PROJECTS_ROOT}/spice-rack`;
