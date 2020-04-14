7603f7f08a69c028925999add6952367b1ca2670
Mike <perrotta@neosensory.com>
2020-04-14 12:30:11 -0700

    enable transitions to correct locations

--- src/containers/app/AppHomePageMenu.js
+++ src/containers/app/AppHomePageMenu.js
@@ -35,7 +35,6 @@ const Menu = styled.div`
 `;
 
 const MenuHashLink = styled(NavHashLink)`
-  ${(props) => props.pos};
   color: ${NEUTRALS.DARK_TEXT};
   font-size: 24px;
   font-style: normal;
@@ -44,7 +43,10 @@ const MenuHashLink = styled(NavHashLink)`
   top: calc(50% - 36px / 2);
   position: absolute;
   text-decoration: none;
-  transition: left 0.3s;
+  transition: left 0.3s, opacity 0.3s;
+  opacity: 1;
+
+  ${(props) => props.pos};
 
   &:hover {
    color: ${NEUTRALS.BLACK};
@@ -57,16 +59,21 @@ const projWidth = 84.56;
 const aboutWidth = 62.5;
 const menuMargin = 48;
 
-const titleCentered = `left: calc(50% - ${titleWidth}px / 2)`;
-const titleLeft = `left: calc(${menuMargin}px)`;
-const projRight = `left: calc(100% - calc(${projWidth}px + ${aboutWidth}px + calc(${menuMargin}px * 2)))`
-const projLeft = `left: calc(${titleWidth}px + calc(${menuMargin}px * 2))`;
-const aboutRight = `left: calc(100% - calc(${aboutWidth}px + ${menuMargin}px))`;
-const aboutLeft = `left: calc(${titleWidth}px + ${projWidth}px + calc(${menuMargin}px * 3))`;
+const titleCenter = css`left: calc(50% - ${titleWidth}px / 2);`;
+const titleInvisible = css`
+  opacity: 0;
+  ${titleCenter}
+`;
+const titleLeft = css`left: calc(${menuMargin}px);`;
+const projRight = css`left: calc(100% - calc(${projWidth}px + ${aboutWidth}px + calc(${menuMargin}px * 2)));`;
+const projCenter = css`left: calc(50% - ${projWidth}px / 2);`;
+const projLeft = css`left: calc(${titleWidth}px + calc(${menuMargin}px * 2));`;
+const aboutRight = css`left: calc(100% - calc(${aboutWidth}px + ${menuMargin}px));`;
+const aboutLeft = css`left: calc(${titleWidth}px + ${projWidth}px + calc(${menuMargin}px * 3));`;
 
 const AppHomePageMenu = () => {
   const [isSticky, setStickiness] = useState(false);
-  const [titlePos, setTitlePos] = useState(titleCentered);
+  const [titlePos, setTitlePos] = useState(titleInvisible);
   const [projPos, setProjPos] = useState(projRight);
   const [aboutPos, setAboutPos] = useState(aboutRight);
   const ref = useRef({});
@@ -74,9 +81,19 @@ const AppHomePageMenu = () => {
     if (ref.current) {
       const menuPos = ref.current.getBoundingClientRect().top;
       setStickiness(menuPos < 0);
-      setTitlePos(menuPos < 0 ? titleLeft : titleCentered);
-      setProjPos(menuPos < 0 ? projLeft : projRight);
-      setAboutPos(menuPos < 0 ? aboutLeft : aboutRight);
+      if (menuPos >= -185) {
+        setTitlePos(titleInvisible);
+        setProjPos(projRight);
+      }
+      else if (menuPos - 72 >= -1 * window.innerHeight) {
+        setTitlePos(titleCenter);
+        setProjPos(projRight);
+      }
+      else {
+        setTitlePos(titleLeft);
+        setProjPos(projCenter);
+      }
+      // setAboutPos(menuPos < 0 ? aboutLeft : aboutRight);
     }
   };
   useEffect(() => {
