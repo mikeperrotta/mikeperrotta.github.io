1d765904032393d9132e3b4b943d8483f732a7c1
Mike <perrotta@neosensory.com>
2020-04-18 13:50:57 -0700

    remove spice and shoe rack projects

--- src/containers/app/AppContainer.js
+++ src/containers/app/AppContainer.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import AppHomePage from './AppHomePage';
-import SpiceRack from '../projects/spicerack/SpiceRack';
 import EmpireApp from '../projects/empire_app/EmpireApp';
 import * as Routes from '../../core/router/Routes';
 
 const AppContainer = () => (
   <Switch>
     <Route path={Routes.EMPIRE_APP} component={EmpireApp} />
-    <Route path={Routes.SPICE_RACK} component={SpiceRack} />
     <Route component={AppHomePage} />
   </Switch>
 );
