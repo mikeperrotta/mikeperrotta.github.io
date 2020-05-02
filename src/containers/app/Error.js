318fdce531701858ce2c32b327579c06665aa4a5
Mike <perrotta@neosensory.com>
2020-05-02 09:57:20 -0700

    Update Error.js

--- src/containers/app/Error.js
+++ src/containers/app/Error.js
@@ -20,8 +20,8 @@ const Content = styled.div`
 `;
 
 const HeaderText = styled.div`
-  font-size: 42px;
-  font-weight: 400;
+  font-size: 60px;
+  font-weight: 200;
 `;
 
 const ImageContainer = styled.div`
@@ -32,7 +32,7 @@ const ImageContainer = styled.div`
 `;
 
 const Image = styled.img`
-  margin: 40px;
+  margin: 60px;
   width: 900px;
   max-width: 80%;
 
@@ -59,7 +59,7 @@ const ErrorPage = () => (
   <>
     <AppHomePageMenu projectMenu />
     <Content>
-      <HeaderText>404 Error: Page Not Found!</HeaderText>
+      <HeaderText>404!</HeaderText>
       <ImageContainer>
         <Image src={ManyMikes} alt="" />
       </ImageContainer>
