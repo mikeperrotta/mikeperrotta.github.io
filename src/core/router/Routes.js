a35f285c67362b312bd333b0ba6a2c2d03d6c0cd
Mike <perrotta@neosensory.com>
2021-02-25 00:12:29 -0800

    update medium link

--- src/core/router/Routes.js
+++ src/core/router/Routes.js
@@ -18,7 +18,7 @@ export const ACTIVITY_TRACKER = '/tracker';
 export const EMPIRE_APP_APP_STORE = 'https://apps.apple.com/us/app/empire-party-game/id1494647723';
 export const EMPIRE_APP_TUTORIAL = 'https://blog.bitsrc.io/building-a-react-native-app-with-expo-530ddef0e1ab?source=friends_link&sk=e706805a23efbbe25c11d0e09964d12c';
 export const MEDITATION_ARTICLE = 'https://www.medium.com/better-humans/shaping-your-meditation-through-brain-science-ddd33b0fe2ad?source=friends_link&sk=8bc68d28a599bd097120fef820bb8c54';
-export const MEDIUM_PROFILE = 'https://www.medium.com/@michaelvperrotta';
+export const MEDIUM_PROFILE = 'https://mikeperrotta.medium.com';
 export const NEOSENSORY_EXTERNAL = 'https://neosensory.com';
 export const NEOSENSORY_BUZZ_POST = 'https://neosensory.com/blog/introducing-buzz';
 export const NEOSENSORY_TED_TALK = 'https://www.ted.com/talks/david_eagleman_can_we_create_new_senses_for_humans';
